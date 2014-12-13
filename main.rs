0964a8392b3bd8f8b5402d93df941e2bea441c33
Rootul Patel <rootulp@gmail.com>
2014-12-13 03:40:22 -0500

    playing with tuples

--- main.rs
+++ main.rs
@@ -1,5 +1,8 @@
+fn next_two(x: int) -> (int, int) { 
+  (x + 1i, x + 2i) 
+}
+
 fn main() {
-  let x = 5i;
-  let y = if x == 5i { 10i } else { 15i };
-  println!("value of y is {}", y)
+    let (x, y) = next_two(5i);
+    println!("The numbers after 5 are: {}, {}", x, y);
 }
