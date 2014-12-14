6078746f2f381c19ab3fa4d92be91bb082d314f4
Rootul Patel <rootulp@gmail.com>
2014-12-14 06:15:49 -0500

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,35 +2,34 @@ use std::io;
 use std::rand;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess a number between 1 and 10.");
 
-    let secret_number = (rand::random::<uint>() % 100u) + 1u;
+    let secret_number = (rand::random::<uint>() % 10u) + 1u;
 
-    println!("The secret number is: {}", secret_number);
+    loop {
 
-    println!("Please input your guess.");
+        let input = io::stdin()
+                            .read_line()
+                            .ok()
+                            .expect("Failed to read line");
+        let input_num: Option<uint> = from_str(input.as_slice().trim());
 
-    let input = io::stdin()
-                        .read_line()
-                        .ok()
-                        .expect("Failed to read line");
-    let input_num: Option<uint> = from_str(input.as_slice().trim());
+        let num = match input_num {
+            Some(num) => num,
+            None      => {
+                println!("Please input a number");
+                continue;
+            }
+        };
 
-    let num = match input_num {
-        Some(num) => num,
-        None => {
-            println!("Please input a number!");
-            return;
+        match cmp(num, secret_number) {
+            Less    => println!("Too small! Keep guessing."),
+            Greater => println!("Too big! Keep guessing."),
+            Equal   => {
+                println!("You win!");
+                return;
+            },
         }
-    };
-
-
-    println!("You guessed: {}", num);
-
-    match cmp(num, secret_number) {
-        Less    => println!("Too small!"),
-        Greater => println!("Too big!"),
-        Equal   => println!("You win!"),
     }
 }
 
