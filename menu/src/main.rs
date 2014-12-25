e60278a99796a65a54d0782bd6471a73e1635c57
Rootul Patel <rootulp@gmail.com>
2014-12-25 10:27:26 -0500

    pull out next input and match logic from select

--- menu/src/main.rs
+++ menu/src/main.rs
@@ -16,17 +16,25 @@ fn print_both(menu: [&str, ..4], prompt: &str) {
 }
 
 // Grab the next line of input
-fn next_input() -> Option<int> {
+fn next_input() -> int {
 
     let mut stdin = io::stdin();
-    let err_line: io::IoResult<String> = stdin.read_line();
-    let line: String = err_line.unwrap();
-    let line_no_extra_whitespace: &str = line.as_slice().trim();
+    let line: io::IoResult<String> = stdin.read_line();
+    let string: String = line.unwrap();
+    let trimmed: &str = string.as_slice().trim();
+    let pos_num: Option<int> = from_str(trimmed);
 
-    // Return potential number
-    return from_str(line_no_extra_whitespace);
-
+    match pos_num {
+        Some(num) => 
+            match num {
+                0...3 => return num,
+                 _=> return -1
+            },
+        None => return -1
+    }
 }
+
+
 
 // Couldn't figure out how to make select return an element of menu
 // Got a 'missing lifetime specifier' error when trying to return &str
@@ -35,14 +43,13 @@ fn select(menu: [&str, ..4], prompt: &str) -> String {
     // Loop until user inputs a valid number
     loop {
         print_both(menu, prompt);
-        let user_input: Option<int> = next_input(); 
-
-        match user_input {
-            Some(num) => match num {
-                0...3 => return menu[num as uint].to_string(),
-                _=> print_both(menu, prompt)
-            },
-            None => print_both(menu, prompt)
+        let numero: int = next_input();
+        match numero{
+            0 => return menu[0 as uint].to_string(),
+            1 => return menu[1 as uint].to_string(),
+            2 => return menu[2 as uint].to_string(),
+            3 => return menu[3 as uint].to_string(),
+            _=> print_both(menu, prompt),
         }
     }
     return "No Solution".to_string();
