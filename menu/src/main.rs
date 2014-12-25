91fc060e3e7177f0f3cb154b6a8dbf4377bdbe5b
Rootul Patel <rootulp@gmail.com>
2014-12-25 10:36:00 -0500

    more comments and better matching

--- menu/src/main.rs
+++ menu/src/main.rs
@@ -18,23 +18,24 @@ fn print_both(menu: [&str, ..4], prompt: &str) {
 // Grab the next line of input
 fn next_input() -> int {
 
+    // Convert it to a possible int
     let mut stdin = io::stdin();
     let line: io::IoResult<String> = stdin.read_line();
     let string: String = line.unwrap();
     let trimmed: &str = string.as_slice().trim();
-    let pos_num: Option<int> = from_str(trimmed);
+    let possible: Option<int> = from_str(trimmed);
 
-    match pos_num {
-        Some(num) => 
-            match num {
-                0...3 => return num,
-                 _=> return -1
-            },
-        None => return -1
+    // If possible num is between 0 and 3, return it. 
+    // Otherwise return -1 <= I don't think this is good practice
+    match possible {
+        None => return -1,
+        Some(possible) => match possible {
+            0...3 => return possible,
+            _=> return -1
+        }
     }
+
 }
-
-
 
 // Couldn't figure out how to make select return an element of menu
 // Got a 'missing lifetime specifier' error when trying to return &str
@@ -43,12 +44,9 @@ fn select(menu: [&str, ..4], prompt: &str) -> String {
     // Loop until user inputs a valid number
     loop {
         print_both(menu, prompt);
-        let numero: int = next_input();
-        match numero{
-            0 => return menu[0 as uint].to_string(),
-            1 => return menu[1 as uint].to_string(),
-            2 => return menu[2 as uint].to_string(),
-            3 => return menu[3 as uint].to_string(),
+        let menu_index: int = next_input();
+        match menu_index{
+            0...3 => return menu[menu_index as uint].to_string(),
             _=> print_both(menu, prompt),
         }
     }
