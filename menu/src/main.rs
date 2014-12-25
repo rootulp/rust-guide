5d4aef2377c9398427919623af38661744306ddc
Rootul Patel <rootulp@gmail.com>
2014-12-25 09:54:55 -0500

    add comments and clean up a little

--- menu/src/main.rs
+++ menu/src/main.rs
@@ -1,48 +1,55 @@
 // Implements http://rosettacode.org/wiki/Menu
 
-fn print_both(menu: [&str, ..4], prompt: &str) {
-    println!("{}", prompt);
+use std::io;
 
-    for i in range(0, 4u) {
+// Print the menu followed by the prompt
+fn print_both(menu: [&str, ..4], prompt: &str) {
+
+    // Iterate through array and print index then period then menu item
+    for i in range(0, 4u) { // Hard coded number 4 here
         println!("{}. {}", i, menu[i]);
     }
+
+    // Print the prompt
+    println!("{}", prompt);
+
 }
 
-fn next_line() -> Option<int> {
-    use std::io;
+// Grab the next line of input
+fn next_input() -> Option<int> {
 
     let mut stdin = io::stdin();
-
     let err_line: io::IoResult<String> = stdin.read_line();
     let line: String = err_line.unwrap();
-
     let line_no_extra_whitespace: &str = line.as_slice().trim();
-    let possible_number: Option<int> = from_str(line_no_extra_whitespace);
-    return possible_number;
+
+    // Return potential number
+    return from_str(line_no_extra_whitespace);
+
 }
 
-fn menu(menu: [&str, ..4], prompt: &str) -> String {
+// Couldn't figure out how to make select return an element of menu
+// Got a 'missing lifetime specifier' error when trying to return &str
+fn select(menu: [&str, ..4], prompt: &str) -> String {
 
-    let mut done = false;
-    while !done {
+    // Loop until user inputs a valid number
+    loop {
         print_both(menu, prompt);
-        let possible_number: Option<int> = next_line(); 
+        let user_input: Option<int> = next_input(); 
 
-        match possible_number {
-            Some(n) => match n {
-                0...3 => return menu[n as uint].to_string(),
+        match user_input {
+            Some(num) => match num {
+                0...3 => return menu[num as uint].to_string(),
                 _=> print_both(menu, prompt)
             },
             None => print_both(menu, prompt)
         }
     }
-    //return menu[n.parse::<uint>()].to_string()
-    return "goodbye".to_string();
+    return "No Solution".to_string();
 }
 
 fn main() {
     let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
     let prompt = "Choose one.";
-    let mut result = menu(items, prompt);
-    println!("{}", result);
+    println!("{}", select(items, prompt));
 }
