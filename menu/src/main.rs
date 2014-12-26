f4e9ac90b892eee54a030e4999049efe8482e5d1
Rootul Patel <rootulp@gmail.com>
2014-12-26 18:53:15 -0500

    better var names and add empty_menu test

--- menu/src/main.rs
+++ menu/src/main.rs
@@ -1,7 +1,6 @@
 // Implements http://rosettacode.org/wiki/Menu
 
 use std::io;
-use std::str;
 
 // Print the menu followed by the prompt
 fn print_both(menu: &[&str], prompt: &str) {
@@ -19,19 +18,13 @@ fn print_both(menu: &[&str], prompt: &str) {
 // Grab the next line of input
 fn next_input() -> Option<uint> {
 
-    // Convert it to a possible int
-    // let mut stdin = io::stdin();
-    // let line = stdin.read_line().unwrap();
-    // let trimmed = line.trim();
-    let input = io::stdin().read_line()
-                           .ok()
-                           .expect("Failed to read line");
-    let user_input: Option<uint> = input.trim().parse();
-
-    return user_input;
+    let line = io::stdin().read_line().unwrap();
+    let input: Option<uint> = line.trim().parse();
+    return input;
 }
 
 fn select<'a>(menu: &'a [&str], prompt: &str) -> &'a str {
+    
     // Check if menu is empty
     if menu.len() == 0 {
         return "";
@@ -42,25 +35,36 @@ fn select<'a>(menu: &'a [&str], prompt: &str) -> &'a str {
 
         print_both(menu, prompt);
 
-        let user_input = next_input();
+        let input = next_input();
 
-        let menu_index = match user_input {
-            Some(menu_index) => menu_index,
+        let num = match input {
+            Some(num) => num,
             None      => continue
         };
 
-        if let 0...3 = menu_index {
-            return menu[menu_index];
+        if let 0...3 = num {
+            return menu[num];
         }
     }
 }
 
 #[cfg(not(test))]
 fn main() {
+
     let prompt = "Choose one.";
     let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
     let menu = items.slice(0,4);
+
     println!("{}", select(menu, prompt));
 }
 
-// Need to add tests but having trouble simulating std io
+
+#[test]
+fn test_empty_menu() {
+    let prompt = "Choose one.";
+    let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
+    let menu = items.slice(0,0);
+    assert_eq!(select(menu, prompt), "");
+}
+
+// Need to add more tests but having trouble simulating std input
